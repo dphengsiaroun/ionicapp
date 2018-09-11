7a49b36875a355e15780867bb8e4f9400af16f9c
PHENGSIAROUN Dany <dphengsiaroun@alliance.net>
2018-09-11 16:19:55 +0200

    add: filter on restaurant menu

--- src/pages/restaurantMenu/restaurantMenu.ts
+++ src/pages/restaurantMenu/restaurantMenu.ts
@@ -2,54 +2,193 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'starter.html'
+	templateUrl: 'starter.html'
 })
 export class StarterContentPage {
-  isAndroid: boolean = false;
+	isAndroid: boolean = false;
+	searchQuery: string = '';
+	starters: Array<{ title: string, resume: string, image: string, fn: string }>;
+	constructor(platform: Platform) {
+		this.isAndroid = platform.is('android');
+		this.initializeStarters();
+	}
 
-  constructor(platform: Platform) {
-    this.isAndroid = platform.is('android');
-  }
+	initializeStarters() {
+		this.starters = [
+			{
+				title: 'Salade Cesar',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/menu.jpg',
+				fn: 'restaurantMenu'
+			},
+			{
+				title: 'Tomate mozarella',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/vins.jpg',
+				fn: 'wineMenu'
+			},
+			{
+				title: 'Oeuf mayonnaise',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/waiter.jpg',
+				fn: 'callWaiter'
+			},
+			{
+				title: 'Carpaccio de boeuf',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/clients.jpg',
+				fn: 'customerReview'
+			},
+		];
+	}
+
+	getItems(event: any) {
+		// Reset items back to all of the items
+		this.initializeStarters();
+
+		// set val to the value of the searchbar
+		const val = event.target.value;
+
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+			this.starters = this.starters.filter((starter) => {
+				return (starter.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+	}
 }
 
 @Component({
-  templateUrl: 'dishes.html'
+	templateUrl: 'dishes.html'
 })
 export class DishesContentPage {
-  isAndroid: boolean = false;
+	isAndroid: boolean = false;
+	searchQuery: string = '';
+	dishes: Array<{ title: string, resume: string, image: string, fn: string }>;
+	constructor(platform: Platform) {
+		this.isAndroid = platform.is('android');
+		this.initializeDishes();
+	}
 
-  constructor(platform: Platform) {
-    this.isAndroid = platform.is('android');
-  }
+	initializeDishes() {
+		this.dishes = [
+			{
+				title: 'Tagliatelle au saumon',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/menu.jpg',
+				fn: 'restaurantMenu'
+			},
+			{
+				title: 'Escalope à la milanese',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/vins.jpg',
+				fn: 'wineMenu'
+			},
+			{
+				title: 'Boeuf bourguignon',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/waiter.jpg',
+				fn: 'callWaiter'
+			},
+			{
+				title: 'Poulet braisé',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/clients.jpg',
+				fn: 'customerReview'
+			},
+		];
+	}
+
+	getItems(event: any) {
+		// Reset items back to all of the items
+		this.initializeDishes();
+
+		// set val to the value of the searchbar
+		const val = event.target.value;
+
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+			this.dishes = this.dishes.filter((dishe) => {
+				return (dishe.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+	}
 }
 
 @Component({
-  templateUrl: 'desserts.html'
+	templateUrl: 'desserts.html'
 })
 export class DessertsContentPage {
-  isAndroid: boolean = false;
 
-  constructor(platform: Platform) {
-    this.isAndroid = platform.is('android');
-  }
+	isAndroid: boolean = false;
+	searchQuery: string = '';
+	desserts: Array<{ title: string, resume: string, image: string, fn: string }>;
+	constructor(platform: Platform) {
+		this.isAndroid = platform.is('android');
+		this.initializeDesserts();
+	}
+
+	initializeDesserts() {
+		this.desserts = [
+			{
+				title: 'Caramel brulé',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/menu.jpg',
+				fn: 'restaurantMenu'
+			},
+			{
+				title: 'Flan',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/vins.jpg',
+				fn: 'wineMenu'
+			},
+			{
+				title: 'Framboisier',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/waiter.jpg',
+				fn: 'callWaiter'
+			},
+			{
+				title: 'Tarte Tatin',
+				resume: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.du restaurant.',
+				image: '../../assets/imgs/clients.jpg',
+				fn: 'customerReview'
+			},
+		];
+	}
+
+	getItems(event: any) {
+		// Reset items back to all of the items
+		this.initializeDesserts();
+
+		// set val to the value of the searchbar
+		const val = event.target.value;
+
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+			this.desserts = this.desserts.filter((dessert) => {
+				return (dessert.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+	}
 }
 
 @Component({
-  template: `
-    <ion-tabs class="tabs-icon-text" [color]="isAndroid ? 'royal' : 'dark'">
-      <ion-tab tabIcon="leaf" tabTitle="Entrées" [root]="starter"></ion-tab>
-      <ion-tab tabIcon="md-restaurant" tabTitle="Plats" [root]="dishes"></ion-tab>
-      <ion-tab tabIcon="md-ice-cream" tabTitle="Desserts" [root]="desserts"></ion-tab>
-    </ion-tabs>
+	template: `
+	<ion-tabs class="tabs-icon-text" [color]="isAndroid ? 'royal' : 'royal'">
+	  <ion-tab tabIcon="leaf" tabTitle="Entrées" [root]="starter"></ion-tab>
+	  <ion-tab tabIcon="md-restaurant" tabTitle="Plats" [root]="dishes"></ion-tab>
+	  <ion-tab tabIcon="md-ice-cream" tabTitle="Desserts" [root]="desserts"></ion-tab>
+	</ion-tabs>
 `})
 export class RestaurantMenuPage {
-  starter = StarterContentPage;
-  dishes = DishesContentPage;
-  desserts = DessertsContentPage;
+	starter = StarterContentPage;
+	dishes = DishesContentPage;
+	desserts = DessertsContentPage;
 
-  isAndroid: boolean = false;
+	isAndroid: boolean = false;
 
-  constructor(platform: Platform) {
-    this.isAndroid = platform.is('android');
-  }
+	constructor(platform: Platform) {
+		this.isAndroid = platform.is('android');
+	}
 }
